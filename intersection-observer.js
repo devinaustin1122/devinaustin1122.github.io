5f5fba8509f8df0279ce8cbff571e1c073f4eb36
dyerda@longwood.edu <dyerda@longwood.edu>
2022-02-01 15:20:44 -0500

    updates

--- intersection-observer.js
+++ intersection-observer.js
@@ -1,8 +1,16 @@
+'use strict';
+
 const sectionAbout = document.querySelector('.about');
+
 
 const options = {
     root: null,
     threshold: .5 // 0 to 1 value
+}; 
+
+const optionsToggle = {
+    root: null,
+    threshold: 0 // 0 to 1 value
 }; 
 
 const observer = new IntersectionObserver(function(entries, observer) {
@@ -10,5 +18,12 @@ const observer = new IntersectionObserver(function(entries, observer) {
     console.log(entry);
  });
 }, options);
+
+const observerToggle = new IntersectionObserver(function(entries, observer) {
+    entries.forEach(entry => {
+       console.log(entry);
+    });
+   }, optionsToggle);
+   
 
 observer.observe(sectionAbout);
