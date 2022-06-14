0741f32a73b02b7a1a316b677b8bd366af86f1cf
dyerda@longwood.edu <dyerda@longwood.edu>
2022-06-14 10:12:33 -0400

    update js

---
+++ index.js
@@ -0,0 +1,44 @@
+ScrollReveal().reveal(".hero", {
+  duration: 1500,
+  distance: "20px",
+});
+ScrollReveal().reveal(".experience", {
+  duration: 1500,
+  distance: "20px",
+  viewOffset: {
+    bottom: 200,
+  },
+});
+ScrollReveal().reveal(".projects", {
+  duration: 1500,
+  distance: "20px",
+  viewOffset: {
+    bottom: 200,
+  },
+});
+
+function toggleMenu() {
+  let items = document.getElementById("navigation__items");
+  items.classList.toggle("navigation__items--open");
+}
+
+function handleSelect(e) {
+  let selectors = document.getElementsByClassName(
+    "experience__selector--selected"
+  );
+  selectors[0].classList.toggle("experience__selector--selected");
+  e.target.classList.toggle("experience__selector--selected");
+
+  let descriptions = document.getElementsByClassName(
+    "experience__description--selected"
+  );
+
+  descriptions[0].classList.toggle("experience__description--selected");
+
+  let description = document.getElementById(e.target.getAttribute("control"));
+  description.classList.toggle("experience__description--selected");
+}
+
+function setTheme(theme) {
+  document.documentElement.className = theme;
+}
